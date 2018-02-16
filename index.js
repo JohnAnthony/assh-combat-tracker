6cb92b4bbfde203e4a1262a80ce4cf290702f4cb
John Anthony <john@jo.hnanthony.com>
2018-02-16 20:28:59 +0000

    Phase and round changes

--- index.js
+++ index.js
@@ -14,4 +14,28 @@ app.use(express.static(__dirname + '/public'))
 app.use(webpackDevMiddleware(webpack(webpackConfig)))
 app.use(bodyParser.urlencoded({ extended: false }))
 
+io.on('connection', socket => {
+	let state = { round: 0, phase: 0, order: 0 }
+
+	socket.on('roundUp', () => {
+		state = { round: state.round + 1, phase: 0, order: 0 }
+		socket.emit('stateChange', state)
+	})
+
+	socket.on('roundDown', () => {
+		state = { round: Math.max(state.round - 1, 0), phase: 0, order: 0 }
+		socket.emit('stateChange', state)
+	})
+
+	socket.on('phaseUp', () => {
+		state = { ...state, phase: state.phase + 1 }
+		socket.emit('stateChange', state)
+	})
+
+	socket.on('phaseDown', () => {
+		state = { ...state, phase: Math.max(state.phase - 1, 0) }
+		socket.emit('stateChange', state)
+	})
+})
+
 server.listen(3000)
--- src/index.js
+++ src/index.js
@@ -3,12 +3,37 @@ import ReactDOM from 'react-dom'
 import io from 'socket.io-client'
 
 class App extends React.Component {
+	constructor(props) {
+		super(props)
+		this.state = {
+			round: 0,
+			phase: 0,
+			order: 0
+		}
+	}
+
+	componentDidMount() {
+		this.socket = io('/')
+		this.socket.on('stateChange', state => this.setState(state))
+	}
+
+	roundUp = () => this.socket.emit('roundUp')
+	roundDown = () => this.socket.emit('roundDown')
+	phaseUp = () => this.socket.emit('phaseUp')
+	phaseDown = () => this.socket.emit('phaseDown')
+
 	render() {
-		return (
-			<div>
-				<h1>Hello, World!</h1>
-			</div>
-		)
+		const state = this.state;
+		return <div>
+			<h1>Round: {state.round}</h1>
+			<h1>Phase: {state.phase}</h1>
+			<h1>Order: {state.order}</h1>
+
+			<button onClick={this.roundUp}>Round Up</button>
+			<button onClick={this.roundDown}>Round Down</button>
+			<button onClick={this.phaseUp}>Phase Up</button>
+			<button onClick={this.phaseDown}>Phase Down</button>
+		</div>
 	}
 }
 
